22744289463d70c3bec54a6f1a5929eaf3d8fa04
legendZHANG0915 <3197718055@qq.com>
2021-07-20 15:26:53 +0800

    完善了服务层所有接口和实现类

--- src/main/java/com/cn/online_exam/service/impl/AdminServiceImpl.java
+++ src/main/java/com/cn/online_exam/service/impl/AdminServiceImpl.java
@@ -1,6 +1,9 @@
 package com.cn.online_exam.service.impl;
 
+import com.cn.online_exam.dao.AdminDao;
 import com.cn.online_exam.pojo.Administrator;
+import com.cn.online_exam.pojo.Student;
+import com.cn.online_exam.pojo.Teacher;
 import com.cn.online_exam.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,12 +12,51 @@ import java.util.List;
 
 @Service("adminService")
 public class AdminServiceImpl implements AdminService {
-
     @Autowired
-    private com.cn.online_exam.dao.AdminDao adminDao;
+    private AdminDao adminDao;
 
     @Override
     public List<Administrator> findALL() {
         return adminDao.findAllAdmin();
+    }
+
+    @Override
+    public Administrator findAdmin(String account) {
+        return adminDao.findAdmin(account);
+    }
+
+    @Override
+    public Teacher findTeach(String account) {
+        return adminDao.findTeach(account);
+    }
+
+    @Override
+    public Student findStu(String account) {
+        return adminDao.findStu(account);
+    }
+
+    @Override
+    public void delAdmin(String account) {
+        adminDao.delAdmin(account);
+    }
+
+    @Override
+    public void delTeacher(String account) {
+        adminDao.delTeacher(account);
+    }
+
+    @Override
+    public void delStudent(String account) {
+        adminDao.delStudent(account);
+    }
+
+    @Override
+    public void updateAdmin(Administrator administrator) {
+        adminDao.updateAdmin(administrator);
+    }
+
+    @Override
+    public void addAdmin(Administrator administrator) {
+        adminDao.addAdmin(administrator);
     }
 }
